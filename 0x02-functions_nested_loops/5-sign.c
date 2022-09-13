ad333df4aa1a7fa80cc3cdaffb1bd8f13245dcf3
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-09-13 16:07:21 -0700

    please work

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign - indicates whether int is positive or negative
- * @n: integer value to be tested
- * Return: 1 and prints + if n is greater than zero
- *     Returns 0 and prints '0' if n is zero
- *     Returns -1 and prints '-' if ne is less than zero
- *
- */
+  * print_sign - indicates whether int is positive or negative
+  * @n: integer value to be tested
+  * Return: 1 and prints + if n is greater than zero
+  *     Returns 0 and prints '0' if n is zero
+  *     Returns -1 and prints '-' if ne is less than zero
+  *
+  */
 int print_sign(int n)
 {
 	if (n > 0)
