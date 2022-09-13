d8f526645e7e0491128d57beb056c374b803d85b
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-09-13 16:03:57 -0700

    it should work

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -13,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar ('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar ('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar('0');
+		_putchar ('0');
 		return (0);
 	}
 }
